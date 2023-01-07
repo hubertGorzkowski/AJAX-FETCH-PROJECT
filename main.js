9b1a8158686e9604ba3fcb7fb34c90d91c50632b
Hubert Gorzkowski <h-gorzkowski1@wp.pl>
2023-01-07 15:28:36 +0100

    add method fetch

---
+++ main.js
@@ -0,0 +1,8 @@
+const getUsers = () => {
+  const url = "https://randomuser.me/api/?results=10";
+  fetch(url)
+    .then((response) => console.log(response))
+    .catch((err) => console.log(err));
+};
+
+document.querySelector("button").addEventListener("click", getUsers);
